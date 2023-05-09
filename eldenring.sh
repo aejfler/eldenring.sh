34006fb41ae9a96e3e5ccb816c0c789ed4bd3ecf
Aga <101977156+aejfler@users.noreply.github.com>
2023-05-09 20:55:08 +0200

    Updated  eldenring.sh script

--- eldenring.sh
+++ eldenring.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+echo "Welcome in Elden Ring tarnished!. Please select your starting character:
+1 - Samurai
+2 - Prisoner
+3 - Prophet"
+
+read character
+
+case $character in 
+	1)
+		type="Samurai"
+		hp=100
+		attack=20
+		;;
+	2) 
+		type="Prisoner" 
+                hp=30
+                attack=5 
+                ;; 
+	3)
+		type="Prophet" 
+                hp=50
+                attack=10 
+                ;; 
+esac
+
+echo "You've chosen the $type character. Your HP is $hp and your attack is $attack."
+sleep 1
+
 echo "The battle is comming"
 sleep 1
 
@@ -22,8 +50,11 @@ echo "Your battle starts now!!! Pick a number 0 or 1 ( 0 / 1)"
 
 read tarnished
 
-if [[ $beast == $tarnished ]]; then
+if [[ $beast == $tarnished || $tarnished == "cheatcode" ]]; then
 	echo "You've smashed it! Victory suits you!! Congrats"
+	attack=$((attack + 10))
+
+	echo "Your attack strenghten and now is $attack"
 else
 	echo "You died miserably"
 	exit 1
@@ -39,12 +70,15 @@ read tarnished
 
 beast=$(( $RANDOM % 10 ))
 
-if [[ $beast == $tarnished || $tarnished == "sheatcode"]]; then
+if [[ $beast == $tarnished ]]; then
         if [[ $USER == "root" ]]; then
-                echo "You've concured Margit! Earned respect on battlefield!! Congrats"
-        fi
+		echo "You've concured Margit! Earned respect on battlefield!! Congrats"
+	fi
+	attack=$((attack + 20))
+	echo "Your attack strenghten and now is $attack"
+
+
 else
-        echo "Margit smashed you like a piece of ****!!! Good bye!"
+        echo "Margit smashed you!!! You died!"
         exit 1
-
-
+fi
